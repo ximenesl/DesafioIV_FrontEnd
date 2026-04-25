ca264efdd3d707c0ce5a2d9aaa022ccf6862627e
Lucas Ximenes <82716172+ximenesl@users.noreply.github.com>
2026-04-25 16:52:07 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL;
+const API_URL = https://desafioiv-backend.onrender.com/api/transactions;
 
 const form = document.getElementById('transaction-form');
 const transactionId = document.getElementById('transaction-id');
